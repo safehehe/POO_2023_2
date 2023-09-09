117e855195f16f0a1d98f331243c543218db7fc8
laurarodriguez10 <143963553+laurarodriguez10@users.noreply.github.com>
2023-09-09 12:51:09 -0500

    Add files via upload

---
+++ Corte_1/Clase/Función de Padovan.js
@@ -0,0 +1,21 @@
+function padovan(n){
+   let t_0=1;
+   let t_1=1;
+   let t_2=1;
+   let fin=0;
+   if (n<=2){
+     return 1;
+   }else{
+    
+      for (let i = 3; i <= n; i++) {
+        fin = t_0 + t_1;
+        t_0 = t_1;
+        t_1 = t_2;
+        t_2 = fin;
+      }
+  
+      return fin;
+    }
+   }
+ const n=5
+ console.log(padovan(n))  
